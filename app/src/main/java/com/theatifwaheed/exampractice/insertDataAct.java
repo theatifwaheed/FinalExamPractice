950082ee36d2f7be53f997237c02334d90237a34
TheAtifWaheed <theatifwaheed@gmail.com>
2021-12-29 21:35:45 +0500

    start

---
+++ app/src/main/java/com/theatifwaheed/exampractice/insertDataAct.java
@@ -0,0 +1,14 @@
+package com.theatifwaheed.exampractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class insertDataAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_data);
+    }
+}
